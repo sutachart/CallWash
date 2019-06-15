4a37487f99f951c6ff86d4b0ccafbc5441923cb7
wittaya kongkrapan <k_wittaya@kkumail.com>
2019-06-15 14:29:34 +0700

    Revert "new"
    This reverts commit 67318695654709bb384778bb7fa7925ccbb40743.

--- src/app/call-wash/call-wash.page.ts
+++ src/app/call-wash/call-wash.page.ts
@@ -5,7 +5,6 @@ import { Observable } from 'rxjs';
 import { Http } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -15,35 +14,39 @@ import { AlertController } from '@ionic/angular';
 })
 export class CallWashPage {
 
-  dataReturned: any;
-  datetime: string;
+  dataReturned:any;
   i: any = 0;
+
+  //Address
+  location_name = "บ้าน";
+  addess = "888/37 ต. ในเมือง อ.เมือง จ.ขอนแก่น";
+  acceptant_name = "วิทยา";
+  tel = "0954795579";
+  location_detail = "ซอยหลังมอ ทางเข้าร้านจ๊ะโอ๋น";
 
   //Services
   price: any;
   place: any;
   select_choice: any = '';
-  sel: any = '';
 
   choice = [{
     choice_id: 1,
-    choice_name: 'ซัก',
+    choice_name: 'ซักผ้า',
     choice_price: 50
   }, {
     choice_id: 2,
-    choice_name: 'อบ',
+    choice_name: 'อบผ้า',
     choice_price: 50
   }, {
     choice_id: 3,
-    choice_name: 'รีด',
+    choice_name: 'รีดผ้า',
     choice_price: 50
   }]
 
-  constructor(public modalController: ModalController,
-    private router: Router,
-    public navHttp: Http,
-    public http: HttpClient,
-    private alertController: AlertController) {
+  constructor(public modalController: ModalController, 
+    private router: Router, 
+    public navHttp: Http, 
+    public http: HttpClient) {
 
   }
 
@@ -51,6 +54,13 @@ export class CallWashPage {
     const modal = await this.modalController.create({
       component: ModalPagePage,
       componentProps: {
+        location_name: this.location_name,
+        addess: this.addess,
+        acceptant_name: this.acceptant_name,
+        tel: this.tel,
+        location_detail: this.location_detail,
+        "paramID": 123,
+        "paramTitle": "Test Title"
       }
     });
     // await modal.present();
@@ -65,10 +75,8 @@ export class CallWashPage {
     return await modal.present();
   }
 
-  // Cal function
   calculateService(CValue) {
     this.select_choice = '';
-
     // Price
     this.place = CValue.length * 50;
     this.price = this.place;
@@ -77,59 +85,23 @@ export class CallWashPage {
     for (this.i = 0; this.i < 3; this.i++)
       if (CValue[this.i] != undefined)
         this.select_choice += CValue[this.i];
+    //console.log(CValue[this.i]+CValue[this.i+1]+CValue[this.i+2]);
 
-    if (this.select_choice == 1) this.sel = 'ซัก';
-    if (this.select_choice == 12) this.sel = 'ซัก,อบ';
-    if (this.select_choice == 13) this.sel = 'ซัก,รีด';
-    if (this.select_choice == 123) this.sel = 'ซัก,อบ,รีด';
-    if (this.select_choice == 2) this.sel = 'อบ';
-    if (this.select_choice == 23) this.sel = 'อบ,รีด';
-    if (this.select_choice == 3) this.sel = 'รีด';
-    console.log('Your choice :', this.sel);
     console.log('Your select :', this.select_choice);
-
-    //  Current Date time
-    let date = new Date().toLocaleString();
-    this.datetime = date;
-    console.log(this.datetime);
   }
 
-  // Alert confirm
-  async presentAlert() {
-    const alert = await this.alertController.create({
-      header: 'ยืนยันการเรียก',
-      // subHeader: 'Sub header',
-      message: 'ราคา :' + this.place + '<br>บริการ :' + this.sel,
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel'
-        },
-        {
-          text: 'Confirm',
-          role: 'confirm',
-          handler: () => {
-            console.log("Confirm!");
-            let url: string = "http://localhost/ionicApp/insert_service.php";
-            let dataJson = new FormData();
-            dataJson.append('serv_price', this.price); // total Price
-            dataJson.append('serv_choice', this.select_choice); // total Choice
-            dataJson.append('serv_date', this.datetime); // date time
 
-            let data: Observable<any> = this.http.post(url, dataJson)
-            data.subscribe(res => {
-              if (res != null)
-                console.log(res);
-            });
+  fnInsert() {
+    let url: string = "http://localhost/ionicApp/insert_service.php";
+    let dataJson = new FormData();
+    dataJson.append('serv_price', this.price); // total Price
+    dataJson.append('serv_choice', this.select_choice); // total Choice
 
-            this.router.navigateByUrl('/home');
-          }
-        }
-
-      ]
+    let data: Observable<any> = this.http.post(url, dataJson)
+    data.subscribe(res => {
+      if (res != null)
+        console.log(res);
     });
-    await alert.present();
   }
-
 
 }
